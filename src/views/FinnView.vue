3c66a6babfa27390346e70dd8d08c15c660b4367
Finn <fhertsch235@gmail.com>
2024-03-12 11:58:01 +0100

    vue-start

---
+++ src/views/FinnView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="finn">
+    <h1>This is an about finn page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .finn {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
